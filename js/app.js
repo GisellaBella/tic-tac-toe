8f8888b4f2e675978fa92828d762bb54bdb1465b
Gisellabella <gisella@gmail.com>
2016-11-12 16:37:03 -0700

    latest

--- js/app.js
+++ js/app.js
@@ -1,34 +1,32 @@
 
 window.onload=function(){
 
-console.log ("so far so good");
+  console.log ("so far so good");
 
-var test = document.getElementById("messageBox");
-test.textContent = "testing"; //testing to make sure my div are being called.
+  //var test = document.getElementById("messageBox");
+  //test.textContent = "testing"; //testing to make sure my div are being called.
 
-function newGame() { //function that creates a new game.
-  document.hoozTurn = "X";//starting with player X
-  setMessage ("<h1> Let's Play! </h1>");
-}
+  function newGame() { //function that creates a new game.
+    document.hoozTurn = "X";//starting with player X
+    setMessage ("<h1> Let's Play! </h1>");
+  }
 
-function setMessage(msg) {
-  document.getElementById("messageBox");
-  messageBox.innerHTML = msg;
-}
+  function setMessage(msg) {
+    document.getElementById("messageBox");
+    messageBox.textContent = msg;
+  }
 
-function changePlayer() {
-  if (document.hoozTurn=="X")//checking on the condition of who just clicked, x or o
-  {document.hoozTurn = "O";
-  } //changed the player to O
-  else {
-  document.hoozTurn = "X";
-  } //otherwise it is X's turn.
-}
+  function changePlayer() {
+    if (document.hoozTurn=="X")//checking on the condition of who just clicked, x or o
+    {document.hoozTurn = "O";
+    } //changed the player to O
+    else {
+    document.hoozTurn = "X";
+    } //otherwise it is X's turn.
+  }
 
-square.addEventListener ("click", nextMv);
+  function nextMv(square, hoozTurn){ //unsure if i add the onclick thing here or in html. Confusing info on the interwebs.
+    square.innerText = document.hoozTurn;
+  }
 
-function nextMv(){
-document.getElementById("square");
-square.innerText = document.hoozTurn;
-}
 };
