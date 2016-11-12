02593d848a17c4ef962c304c8f5f7a0c5712b2e4
Gisellabella <gisella@gmail.com>
2016-11-12 16:04:19 -0700

    latest

--- js/app.js
+++ js/app.js
@@ -4,30 +4,31 @@ window.onload=function(){
 console.log ("so far so good");
 
 var test = document.getElementById("messageBox");
-test.textContent = "testing";
-var text = test.textContent;
+test.textContent = "testing"; //testing to make sure my div are being called.
 
 function newGame() { //function that creates a new game.
-document.hoozTurn = "X";//starting with player X
-setMessage ("<h1> Let's Play! </h1>");
+  document.hoozTurn = "X";//starting with player X
+  setMessage ("<h1> Let's Play! </h1>");
 }
 
-console.log ("message should have loaded");
-
 function setMessage(msg) {
-document.getElementById("messageBox");
-messageBox.innerHTML = msg;
+  document.getElementById("messageBox");
+  messageBox.innerHTML = msg;
 }
 
 function changePlayer() {
-if (document.hoozTurn=="X")//checking on the condition of who just clicked, x or o
-document.hoozTurn = "O"; //changed the player to O
-else document.hoozTurn = "X"; //otherwise it is X's turn.
-} 
-
-function nextMv(square){
-  square.innerText= document.hoozTurn;
+  if (document.hoozTurn=="X")//checking on the condition of who just clicked, x or o
+  {document.hoozTurn = "O";
+  } //changed the player to O
+  else {
+  document.hoozTurn = "X";
+  } //otherwise it is X's turn.
 }
 
+square.addEventListener ("click", nextMv);
 
- };
+function nextMv(){
+document.getElementById("square");
+square.innerText = document.hoozTurn;
+}
+};
