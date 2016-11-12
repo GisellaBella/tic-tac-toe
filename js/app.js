6601eaab52f7991b62f2b271a6fe78fc63949f81
Gisellabella <gisella@gmail.com>
2016-11-11 19:29:40 -0700

    latest as of Friday at 7:30

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
+
+document.onload="newGame";
+var whoTurn = ();
+
+function newGame() //function that creates a new game.
+    { document.whoTurn= "X";//starting with player X
+      showPrompt("<h1>Let's Play!</h1>");
+    }
+function showPrompt (prompt){
+  document.getElementById('#prompt').innerHTML=prompt; //function to display message to player using div id prompt
+}
+
+
+
+
+  /*
+
+/*
 /* MY PSUEDO CODE:
 
 we have 9 cells (divs)
 we add event listeners to each div.
 when a div is clicked it adds the div's preset value (from 1-9 to current player's array) 
-		game board with preset values
-			123
-			456
-			789
+    game board with preset values
+      123
+      456
+      789
 
 if this array matches a winning array set, the user gets an alert that either 
 they won or it is the others players move.
 
-123
+123     
 456
 789
 
@@ -32,12 +50,19 @@ winningCombos =
 
 if the total cells played array's  length % 2- move goes to the 'O' Player's array. else it will be X.
 
-*/
+var newGame = ();//create new board{
+   onclick.};
+var start = document.createElement('div');
+
+start.setAttribute('id', ’turn’);}
+
+
 var gameState = function (arrayOne, arrayTwo) {
 /* to be calculated comparing the current players array state against the winning combos.
- If there's a match, game over.*/
-
+ If there's a match, game over.
 };
+var arrayOne = ();
+var arrayTwo = ();
 
 
 var player1Selections = arrayOne[];
@@ -46,23 +71,13 @@ var player2Selections = arrayTwo[];
 var currentPlayer= function(){
 if (totalArray.length % 2  == 0) {
       /* then currentPlayer marks a O;}
-  else currentPlayer marks an X */}      
+  else currentPlayer marks an X }      
 
         
-function newGame() {
-
-var start = document.createElement('div');
-
-start.setAttribute('id', ’turn’);
+function newGame() 
 
 
 turn.addEventListener('click', markX);
 
-
-
 start.appendChild(turn);
-
-}
-
-var markX = function (currentPlayer,adds cell value to array?)
-
+var markX = function (currentPlayer,adds cell value to array?)*/
