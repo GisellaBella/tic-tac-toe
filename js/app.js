05ce042ab24c60752ec42e8b14a31853cfa025ad
Gisellabella <gisella@gmail.com>
2016-11-16 10:37:59 -0700

    getting closer

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ window.onload=function(){
   //test.textContent = "testing"; //testing to make sure my div are being called.
 
 function newGame() { //function that creates a new game.
-var Turn = "X";//starting with player X, Who's turn is it, X or O
 setMessage ("<h1> Let's Play! </h1>");
 }
 
@@ -13,40 +12,50 @@ newGame();
 
 var msg;
 function setMessage(msg) {
-document.getElementById("messageBox");
-messageBox.innerHTML = msg;
-  }
+  document.getElementById("messageBox");
+  messageBox.innerHTML = msg;
+}
 
-var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],
-[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
 var squares = document.getElementsByTagName('td');
-var xSquares= []; //X's click box ID's
-var oSquares= []; //O's Click box ID's
 
-var squaresArray= [];//do i want one array for everything, or one for X and one for O?
+var inPlayArray= []; //squares that have been played.
 
 function squareListener (){ // adding the event listener for someone clicking on a square
-  for (var i = squares.length-1; i >= 0;i-- )
-  {squares[i].addEventListener('click', setSquare);
-  }}
+  for (var i = squares.length-1; i >= 0;i-- ) {
+    squares[i].addEventListener('click', setSquare);
+  }
+}
 
-function setSquare (event) {//setting the square to X or O, depending on what is in or not in the box.
-if (oSquares.length || xSquares.length === 0) //no one has made a move yet.
-{xSquares.push(event.target.id); //collecting to xSquareres array ID value
-event.target.innerHTML = "<h2> X </h2";} //x always goes first
+function setSquare (event) {  //setting the square to X or O, depending on what is in or not in the box.
+  if (inPlayArray.length === 0) { //no one has made a move yet.
+    inPlayArray.push(event.target.id); //collecting to inPlayArray
+    event.target.innerHTML = "<h2> X </h2";
+    Console.log(inPlayArray);
+  }
+    //x always goes first
+  /*} else if (inPlayArray.length === 1 || 3 || 5 || 7) {
+    inPlayArray.push(event.target.id);  ////collecting to inPlayArray
+    event.target.innerHTML = "<h2> O </h2>";
+    checkForWin(inPlayArray);
+  }
+}
 
-{oSquares.push(event.target.id);  ////collecting to OSquareres array ID value
-     event.target.innerHTML = "<h2> O </h2>";}
+function checkForWin (winningCombinations){
+    for (i=0;i<=winningCombinations.length; i++) 
+      if (winningCombinations[i]==(inPlayArray.toString)) {
+      setMessage ("We have a winner!");
+    }
+    else if (event.target.innerHTML === "O") {
+      setMessage ("<h2> It's X's Move </h2");
+    }
+    else if(event.target.innerHTML === "X") {
+      setMessage ("<h2> It's O's Move </h2");
 
-checkForWin(oSquares);
-setMessage ("<h1>O wins!</h1>");
+    }
+*/
 }
 
 
-  function checkForWin (winningCombinations){
-    for (i=0;i<=winningCombinations.length; i++) {
-      if winningCombinations[i]==(oSquares.toString());
-    }}
-    setMessage (We have a winner!);}
-    
-};
+}
